4a4af08d16bb57320dcca76a6c842d5fd09d35c3
Juan José Arboleda <soyjuanarbol@gmail.com>
2023-11-25 16:35:50 -0500

    Initial commit
    Signed-off-by: Juan José Arboleda <soyjuanarbol@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
+const withMDX = require('@next/mdx')()
+ 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+  output: 'export',
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+}
+ 
+module.exports = withMDX(nextConfig)
