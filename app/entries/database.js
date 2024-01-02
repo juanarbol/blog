e8eace5eafe7e4a19262c5cd9533897807ae0417
Juan José Arboleda <soyjuanarbol@gmail.com>
2024-01-02 15:03:37 -0500

    Publish the first entry ever!
    Signed-off-by: Juan José Arboleda <soyjuanarbol@gmail.com>

--- app/entries/database.js
+++ app/entries/database.js
@@ -2,37 +2,7 @@
 export const articles = [
   {
     slug: 'hello-world',
-    title: 'Hello World',
+    title: 'Hello world',
     publishDate: '2024-01-02',
-  },
-  {
-    slug: 'my-second-post',
-    title: 'Hello World 2',
-    publishDate: '2024-01-03',
-  },
-  {
-    slug: 'my-second-post',
-    title: 'Hello World 2',
-    publishDate: '2024-01-03',
-  },
-  {
-    slug: 'my-second-post',
-    title: 'Hello World 2',
-    publishDate: '2024-01-03',
-  },
-  {
-    slug: 'my-second-post',
-    title: 'Hello World 2',
-    publishDate: '2024-01-03',
-  },
-  {
-    slug: 'my-second-post',
-    title: 'Hello World 2',
-    publishDate: '2024-01-03',
-  },
-  {
-    slug: 'my-second-post',
-    title: 'Hello World 2',
-    publishDate: '2024-01-03',
   },
 ]
